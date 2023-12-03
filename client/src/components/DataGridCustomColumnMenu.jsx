f62b72229ff4eaf689ffbb21539e89c2ef6c6d8f
Amit_KD <amit6363fh@gmail.com>
2023-12-03 14:34:04 +0530

    gg

--- client/src/components/DataGridCustomColumnMenu.jsx
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -1,20 +1,20 @@
 import {
     GridColumnMenuContainer,
-    GridFilterMenuItem,
-   /HideGridColMenuItem,
+    // GridFilterMenuItem,
+    // HideGridColumnMenuItem,
   } from "@mui/x-data-grid";
   
   const CustomColumnMenu = (props) => {
-    const { hideMenu, currentColumn, open } = props;
+    // const { hideMenu, currentColumn, open } = props;
     return (
-     {/*  <GridColumnMenuContainer
-        hideMenu={hideMenu}
-        currentColumn={currentColumn}
-        open={open}
-      >
-       <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
-        <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
-    </GridColumnMenuContainer>*/}
+     <GridColumnMenuContainer>
+      {/* //   hideMenu={hideMenu}
+      //   currentColumn={currentColumn}
+      //   open={open}
+      // >
+      //  <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+      //   <HideGridColMenuItem onClick={hideMenu} column={currentColumn} /> */}
+    </GridColumnMenuContainer>
     );
   };
   
